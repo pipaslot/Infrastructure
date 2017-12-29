97937300530435eedac1aeb8ac81b6cb05497198
Petr Stipek <stipek@cngroup.dk>
2017-12-29 13:24:44 +0100

    Role query was replaced by RoleStore

---
+++ Pipaslot.Infrastructure.Security.EntityFramework/RoleStore.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Pipaslot.Infrastructure.Data;
+using Pipaslot.Infrastructure.Data.EntityFramework;
+using Pipaslot.Infrastructure.Security.Data;
+using Pipaslot.Infrastructure.Security.EntityFramework.Entities;
+
+namespace Pipaslot.Infrastructure.Security.EntityFramework
+{
+    public class RoleStore<TKey, TDbContext> : EntityFrameworkRepository<TDbContext, Role<TKey>, TKey>, IRoleStore
+        where TDbContext : DbContext, ISecurityDbContext<TKey>
+    {
+        public RoleStore(IUnitOfWorkFactory uowFactory, IEntityFrameworkDbContextFactory dbContextFactory) : base(uowFactory, dbContextFactory)
+        {
+        }
+
+        public IEnumerable<IRole> GetAll()
+        {
+            return ContextReadOnly.SecurityRole.ToList();
+        }
+
+        public object GetGuestRoleIdentifier()
+        {
+            return ContextReadOnly.SecurityRole.Where(r => r.Name.Equals("Guest")).Select(r => r.Id).FirstOrDefault();
+        }
+    }
+}
---
+++ Pipaslot.Infrastructure.Security/Data/IRoleStore.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace Pipaslot.Infrastructure.Security.Data
+{
+    public interface IRoleStore
+    {
+        /// <summary>
+        /// Returns primary key for unauthenticated users (guests)
+        /// </summary>
+        /// <returns></returns>
+        object GetGuestRoleIdentifier();
+        
+        /// <summary>
+        /// Returns all roles
+        /// </summary>
+        /// <returns></returns>
+        IEnumerable<IRole> GetAll();
+    }
+}
