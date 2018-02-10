a90e2478c7efcec1fa8281edca562dcfc26a2946
Petr Stipek <stipek@cngroup.dk>
2018-02-10 12:04:36 +0100

    Created Security Controller in separated Mvc project

---
+++ Pipaslot.Demo/Api/SecurityController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Pipaslot.Infrastructure.Data;
+using Pipaslot.Infrastructure.Security;
+using Pipaslot.Infrastructure.Security.Data;
+
+namespace Pipaslot.Demo.Api
+{
+    /// <summary>
+    /// Permission management
+    /// </summary>
+    public class SecurityController : Infrastructure.Security.Mvc.SecurityController<int>
+    {
+        public SecurityController(IRoleStore roleStore, IPermissionManager<int> permissionManager, IUnitOfWorkFactory unitOfWorkFactory) : base(roleStore, permissionManager, unitOfWorkFactory)
+        {
+        }
+    }
+}
